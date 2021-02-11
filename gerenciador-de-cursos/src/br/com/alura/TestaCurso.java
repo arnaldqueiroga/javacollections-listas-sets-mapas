e23c5dcb91d5b0d0e5d93e73343365a39066d8ad
arnaldqueiroga <arnaldqueiroga@gmail.com>
2021-02-11 19:12:54 -0300

    03 Relacionamentos com coleções

---
+++ gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
@@ -0,0 +1,23 @@
+package br.com.alura;
+
+
+
+public class TestaCurso {
+
+	public static void main(String[] args) {
+		Curso javaColecoes = new Curso("Dominado as coleções do Java",
+				"Paulo Silveira");
+		
+		
+		
+		// javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList", 21));
+		
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criando uma aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
+		
+		System.out.println(javaColecoes.getAulas());
+
+	}
+
+}
